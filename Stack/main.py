bc15ce84ac0b74d88eae7dfa6e4b49c22dcb47d2
ryan <39787691+parven-dev@users.noreply.github.com>
2024-12-02 16:08:32 +0530

    added push method

--- Stack/main.py
+++ Stack/main.py
@@ -4,18 +4,27 @@ class Node:
         self.next = None
         
 class Stack:
-    def __init__(self, value):
-        new_node = Node(value)
-        self.top  =  new_node
+    def __init__(self):
+        self.top  =  None
         self.height  = 1
     
+    def push(self, value):
+        new_node = Node(value)
+        if self.top is None:
+            self.top = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+            
     def display(self):
         temp = self.top
-
         while temp is not None:
             print(temp.value)
             temp = temp.next
     
-s1 = Stack(40)
+s1 = Stack()
+s1.push(50)
+s1.push(60)
+s1.push(60)
 print(s1.display())
     
